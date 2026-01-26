c5a6051a87dda278ebeb834c49eb63ffed83c3cd
IkerEscartin <iee0004@alu.medac.es>
2026-01-26 12:02:20 +0100

    Ruta configurada y layout (1)

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,19 @@
 <?php
+require __DIR__.'/auth.php';
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
+});
+
+Route::fallback(function () {
+    $ruta = request()->url();
+    return "$ruta no existe";
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('main');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
