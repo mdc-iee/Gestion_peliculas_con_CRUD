65944dcd5f0ece2cadda391c08d3474e70d702a2
IkerEscartin <iee0004@alu.medac.es>
2026-01-27 23:28:52 +0100

    Configuración del lenguaje y layout (2)

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,3 +1,19 @@
 <x-layouts.layout>
-
+    <div class="hero bg-base-200 min-h-screen">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+            <img
+                src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                class="max-w-sm rounded-lg shadow-2xl"
+            />
+            <div>
+                <h1 class="text-5xl font-bold">¡Tenemos grandes noticias!</h1>
+                <p class="py-6">
+                    Muy pronto llegan nuevas películas a nuestra cartelera, con estrenos llenos de acción, emoción, risas y
+                    aventuras para todos los gustos. Prepárate para disfrutar historias increíbles en la pantalla grande y
+                    vivir la mejor experiencia de cine.
+                </p>
+                <button class="btn btn-primary">Ver películas</button>
+            </div>
+        </div>
+    </div>
 </x-layouts.layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 require __DIR__.'/auth.php';
 
-use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,3 +16,11 @@ Route::get('/dashboard', function () {
     return view('main');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Ruta del cambio de idioma
+Route::get('/lang/{locale}', function (string $locale) {
+    if (! in_array($locale, ['es', 'en', 'fr', 'ru'])) {
+        abort(400);
+    }
+    session(['locale' => $locale]);
+    return redirect()->back();
+})->name('lang.switch');
