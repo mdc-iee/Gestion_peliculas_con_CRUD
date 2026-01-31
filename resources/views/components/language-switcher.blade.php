839e8e86593acae4218b58122bde2e620816bb15
IkerEscartin <iee0004@alu.medac.es>
2026-01-31 20:55:30 +0100

    Fix botón seleccionar idioma

--- resources/views/components/language-switcher.blade.php
+++ resources/views/components/language-switcher.blade.php
@@ -1,6 +1,6 @@
 <select onchange="window.location.href=this.value" class="bg-gray-300 p-3" name="lang" id="">
     <option value="" disabled>{{__("Selecciona idioma")}}</option>
     @foreach(config("languages") as $code => $content)
-        <option value="{{route("set_lang", $code)}} {{ app()->getLocale() === $code ? 'selected' : '' }}">{{$content['name']}} {{$content['flag']}}</option>
+        <option value="{{route("set_lang", $code)}}" {{app()->getLocale() === $code ? 'selected' : ''}}>{{$content['name']}} {{$content['flag']}}</option>
     @endforeach
 </select>
